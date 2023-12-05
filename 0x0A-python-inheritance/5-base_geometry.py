e3478586e5e8d272f92c00d58925af026b853174
suadcode <suadsalam7@gmail.com>
2023-12-05 08:11:44 -0800

    base_geometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains the function 5-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """
+    This is a Base Geometry class
+    (an empty class)
+    """
+    pass
