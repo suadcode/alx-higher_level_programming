bce34465aabb3c3570ecbe643d20eb9a8d8bffde
suadcode <suadsalam7@gmail.com>
2024-01-17 10:00:08 -0800

    sql

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- This script will lists all the tables of a database in MySQL server.
+-- The database name will be passed as argument of mysql command
+
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- This script will insert a new row in the table first_table (database hbtn_0c_0) in the MySQL server.
+-- The database name will be passed as an argument of the mysql command
+
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
